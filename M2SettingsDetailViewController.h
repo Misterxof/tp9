66065fdc72e1a954f2f313e869b522701eccd396
Misterxof <mishail.lukushevich@gmail.com>
2017-05-27 13:42:35 +0300

    Controller

---
+++ M2SettingsDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  M2SettingsDetailViewController.h
+//  m2048
+//
+//  Created by Danqing on 26.05.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface M2SettingsDetailViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *options;
+@property (nonatomic, strong) NSString *footer;
+
+@end
